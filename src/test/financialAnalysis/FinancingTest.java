f8c0e9518bdd46f29e3219e34c0bbb09d26cae58
MarachLJLL <marachljll@gmail.com>
2023-10-31 16:23:09 -0400

    Began testing loan payment calculations in Financing

--- src/test/financialAnalysis/FinancingTest.java
+++ src/test/financialAnalysis/FinancingTest.java
@@ -10,7 +10,7 @@ class FinancingTest {
     @Test
     void lynndale55MonthlyPayment() {
         Financing downPayment = new Financing(90000, 3.9, 30);
-        Financing downPayment = new Financing(3000000, 5.01, 30);
+        Financing mortgagePayment = new Financing(3000000, 5.01, 30);
     }
 
 }
